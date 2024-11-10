19ca12cb6079b2c9b1ee583719e11b9d9e7e4753
zsoltoroszlany82 <zsolt.oroszlany@playfulsparkle.com>
2024-11-10 13:13:09 +0100

    Italian language

---
+++ src/admin/language/it-it/module/ps_product_category_filter.php
@@ -0,0 +1,32 @@
+<?php
+// Heading
+$_['heading_title']            = 'Playful Sparkle - Product to Category filter';
+$_['heading_fix']              = 'Correggi i bug comuni';
+$_['heading_getting_started']  = 'Iniziare';
+$_['heading_setup']            = 'Configurazione del Product to Category filter';
+$_['heading_troubleshot']      = 'Risoluzione dei problemi comuni';
+$_['heading_contact']          = 'Contatta il supporto';
+
+// Text
+$_['text_extension']           = 'Estensioni';
+$_['text_success']             = 'Successo: Hai modificato il modulo Product to Category filter!';
+$_['text_filter_success']      = 'Successo: I filtri non utilizzati sono stati rimossi con successo!';
+$_['text_edit']                = 'Modifica il modulo Product to Category filter';
+$_['text_getting_started']     = '<p><strong>Panoramica:</strong> L\'estensione Playful Sparkle - Product to Category filter migliora il tuo negozio OpenCart 4.x semplificando la gestione dei filtri all\'interno delle categorie. Al salvataggio delle informazioni del prodotto, questa estensione collega automaticamente i filtri del prodotto alle categorie pertinenti, risparmiando tempo e garantendo coerenza nell\'impostazione dei filtri.</p><p><strong>Requisiti:</strong> OpenCart 4.x+, PHP 7.4+ devono essere installati e configurati correttamente.</p>';
+$_['text_setup']               = '<p>Per configurare l\'estensione, installala e abilitala tramite il pannello di amministrazione di OpenCart. Vai su Estensioni > Moduli, individua il Playful Sparkle - Product to Category filter e fai clic sul pulsante Abilita.</p>';
+$_['text_troubleshot']         = '<ul><li><strong>Non riesco a vedere il pulsante Rimuovi filtri non utilizzati:</strong> Assicurati che l\'estensione sia abilitata o che i gestori degli eventi siano registrati. Fai clic sul pulsante Correggi gestore eventi per registrarlo nuovamente.</li><li><strong>I filtri non vengono aggiunti alla categoria:</strong> Assicurati che l\'estensione sia abilitata o che i gestori degli eventi siano registrati. Fai clic sul pulsante Correggi gestore eventi per registrarlo nuovamente.</li><li><strong>Non vedo i filtri sulla pagina principale:</strong> Devi abilitare il modulo filtro e assegnarlo a un layout.</li></ul>';
+$_['text_contact']             = '<p>Per ulteriori assistenze, contatta il nostro team di supporto:</p><ul><li><strong>Contatto:</strong> <a href="mailto:%s">%s</a></li><li><strong>Documentazione:</strong> <a href="%s" target="_blank" rel="noopener noreferrer">Documentazione utente</a></li></ul>';
+
+// Tab
+$_['tab_general']              = 'Generale';
+$_['tab_help_and_support']     = 'Aiuto e supporto';
+
+// Entry
+$_['entry_status']             = 'Stato';
+
+// Button
+$_['button_remove_filter']     = 'Rimuovi filtri non utilizzati';
+$_['button_fix_event_handler'] = 'Correggi gestore eventi';
+
+// Error
+$_['error_permission']         = 'Attenzione: Non hai il permesso di modificare il modulo Product to Category filter!';
